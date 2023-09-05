f408a877f11c2d239f82b0f8f7bcf8539470e825
Alvin Obonyo <41358283+ALVIN-OBONYO@users.noreply.github.com>
2023-09-05 23:20:16 +0300

    Update PageFooter.jsx

--- swop/src/components/modules/footer/PageFooter.jsx
+++ swop/src/components/modules/footer/PageFooter.jsx
@@ -1,72 +1,89 @@
-/* import React from 'react';
+import React from 'react';
 import watch from './watch.png';
 import { Link } from 'react-router-dom';
-// import {
-//   FacebookButton,
-//   InstagramButton,
-//   LinkButton,
-//   LinkedInButton,
-//   TwitterButton,
-//   YoutubeButton,
-// } from 'components/widgets/Buttons';
+ import {
+     FacebookButton,
+     InstagramButton,
+     LinkButton, 
+     LinkedInButton,
+     TwitterButton,
+     YoutubeButton,
+ } from 'components/widgets/Buttons';
 
-function PageFooter() {
+
+/*function PageFooter() {
   return (
     <>
-      <div className="text-white grid grid-cols-3 bg-black pb-4">
+      <div className='text-white grid grid-cols-3'>
         <div>
         <div className='pt-8 pl-10 flex'>
           <Link to="/" className="shrink-0 grow-0 rounded-full">
-            <img src={watch} width={100} height={100} alt="Feimat Logo" />
+           <img src={watch} width={100} height={100} alt="Feimat Logo" />
           </Link>
-          <p className="pt-10 pl-4 text-3xl"> SWOP </p>
+          <p className='pt-6 text-xl font-medium'>Swop</p>
         </div>
          <p className="text-white font-semibold pt-10 pl-4">
-            {`© Copyright ${new Date().getFullYear()} Swop All rights reserved.`}
+            {`© Copyright ${new Date().getFullYear()} SWOP All rights reserved.`}
           </p>
         </div>
-        <div>
-          <h3 className="font-bold text-2xl mt-5 mb-2 text-white">Navigate</h3>
-          <div className="grid gap-2 grid-cols-2 mt-6">
-            <div className="flex flex-col">
-              <Link to="/" className='text-white'>Home</Link>
-              <Link to="/" className='text-white'>About Us</Link>
-              <Link to="/" className='text-white'>Account</Link>
-            </div>
+      */
+       // <div className='grid gap-2 grid-cols-2 mt-6'>
+						<div className='flex flex-col'>
+							<LinkButton to='/'>Home</LinkButton>
+							<LinkButton to='/'>News</LinkButton>
+							<LinkButton to='/Contact'>Contact</LinkButton>
+							<LinkButton to='/'>Get In Touch</LinkButton>
+						</div>
 
-            <div className="flex flex-col">
-              <Link to="/" className='text-white'>Men Watches</Link>
-              <Link to="/" className='text-white'>Women Watches</Link>
-              <Link to="/" className='text-white'>Kid Watches</Link>
-            </div>
-          </div>
-        </div>
-        <div>
-          <h3 className="font-bold text-2xl mt-5 mb-2 text-white">
-            Contact Us
-          </h3>
-          <div className="text-white">
-            Nairobi, Kenya
-          </div>
-          <div className="text-white">
-            info@swop.co.ke
-          </div>
-          <div className="flex gap-x-4">
-          </div>
-        </div>
+          <div className='flex flex-col'>
+							
+							<LinkButton to='/Donate'>Donate</LinkButton>
+              <LinkButton to='/'>Careers</LinkButton>
+							<LinkButton to='/'>Services</LinkButton>
+							<LinkButton to='/'>Privacy Policy</LinkButton>
+						</div>
+					
+              
+			<div>
+              
+					<h3 className='font-semibold text-2xl mt-5 mb-2 text-white'>
+						Contact Us
+            
+					</h3>
+					<div className='text-white flex gap-x-2'>
+						<LocationButton />
+						 Nairobi, Kenya
+					</div>
+					<div className='text-white flex gap-x-2 pt-4'>
+						<EmailButton />
+						info@swop.co.ke
+					</div>
+					<h3 className='font-semibold text-2xl mt-5 mb-2'>
+						Get In Touch With Us
+					</h3>
+					<div className='flex gap-x-4'>
+						<InstagramButton />
+						<LinkedInButton />
+						<FacebookButton />
+						<TwitterButton />
+						<YoutubeButton />
+					</div>
+          </div>    
       </div>
-      {/* This is the copyright section */}
+  
 
-/*
-      <div className="text-white grid grid-cols-3 w-full bg-black">
-        <div>
-        </div>
-        <div></div>
-        <div></div>
-      </div>
-    </>
-  );
+
+  <div className='text-white grid grid-cols-3 w-full'>
+				<div>
+					<p className='text-white font-normal pt-4'>
+						© Copyright 2023 SWOP All rights reserved.
+					</p>
+				</div>
+				<div></div>
+				<div></div>
+			</div>
+		</>
+	);
 }
 
 export default PageFooter;
-*/
