134901c34aedb2a2ca7fd81e93786e1566ae3d5d
Alvin Obonyo <41358283+ALVIN-OBONYO@users.noreply.github.com>
2023-11-16 19:47:34 +0300

    Update PageFooter.jsx

--- swop/src/components/modules/footer/PageFooter.jsx
+++ swop/src/components/modules/footer/PageFooter.jsx
@@ -1,91 +1,70 @@
-/* import React from 'react';
-import watch from './watch.png';
+import React from 'react';
 import { Link } from 'react-router-dom';
- import {
-     FacebookButton,
-     InstagramButton,
-     LinkButton, 
-     LinkedInButton,
-     TwitterButton,
-     YoutubeButton,
- } from 'components/widgets/Buttons';
+import {
+	FacebookButton,
+	InstagramButton,
+	LinkedInButton,
+	TwitterButton,
+	YoutubeButton,
+} from 'components/widgets/Buttons';
 
-
-/*function PageFooter() {
-  return (
-    <>
-      <div className='text-white grid grid-cols-3'>
-        <div>
-        <div className='pt-8 pl-10 flex'>
-          <Link to="/" className="shrink-0 grow-0 rounded-full">
-           <img src={watch} width={100} height={100} alt="Feimat Logo" />
-          </Link>
-          <p className='pt-6 text-xl font-medium'>Swop</p>
-        </div>
-         <p className="text-white font-semibold pt-10 pl-4">
-            {`© Copyright ${new Date().getFullYear()} SWOP All rights reserved.`}
-          </p>
-        </div>
-      
-        <div className='grid gap-2 grid-cols-2 mt-6'>
-						<div className='flex flex-col'>
-							<LinkButton to='/'>Home</LinkButton>
-							<LinkButton to='/'>News</LinkButton>
-							<LinkButton to='/Contact'>Contact</LinkButton>
-							<LinkButton to='/'>Get In Touch</LinkButton>
-						</div>
-
-          <div className='flex flex-col'>
-							
-							<LinkButton to='/Donate'>Donate</LinkButton>
-              <LinkButton to='/'>Careers</LinkButton>
-							<LinkButton to='/'>Services</LinkButton>
-							<LinkButton to='/'>Privacy Policy</LinkButton>
-						</div>
-					
-              
-			<div>
-              
-					<h3 className='font-semibold text-2xl mt-5 mb-2 text-white'>
-						Contact Us
-            
-					</h3>
-					<div className='text-white flex gap-x-2'>
-						<LocationButton />
-						 Nairobi, Kenya
+function PageFooter() {
+	return (
+		<>
+			<div className='text-white grid grid-cols-3'>
+				<div className='text-center text-white'>
+					<p className='font-semibold text-xl mt-5 pb-6'>REAL PARADISE</p>
+					<p className='pb-2 text-base'>5040 Real Paradise, Kenya</p>
+					<p className='pb-2 text-base'>P.O Box 20227 - 00100</p>
+					<p className='pb-2 text-base'>Nairobi Kenya</p>
+					<p className='pb-2 text-base'>+2547000000</p>
+					<p className='pb-2 text-base'>info@realparadise.com</p>
+				</div>
+				<div className='text-center'>
+					<p className='font-semibold text-xl mt-5 pb-6 text-white'>NAVIGATE</p>
+					<div className='text-base'>
+						<Link to='/'>Home</Link> <br />
+						<Link to='/'>News</Link> <br />
+						<Link to='/'>Careers</Link> <br />
+						<Link to='/Contact'>Contact</Link> <br />
+						<Link to='/'>Get In Touch</Link> <br />
 					</div>
-					<div className='text-white flex gap-x-2 pt-4'>
-						<EmailButton />
-						info@swop.co.ke
+				</div>
+				<div>
+					<div className='text-center'>
+						<p className='font-semibold text-xl mt-5 pb-6 text-white'>
+							EXPLORE
+						</p>
+						<div className='text-base'>
+							<Link to='/Buy'>Buy</Link> <br />
+							<Link to='/Rent'>Rent</Link> <br />
+							<Link to='/'>Developments</Link> <br />
+							<Link to='/'>Services</Link> <br />
+							<Link to='/'>Privacy Policy</Link> <br />
+						</div>
 					</div>
-					<h3 className='font-semibold text-2xl mt-5 mb-2'>
-						Get In Touch With Us
-					</h3>
-					<div className='flex gap-x-4'>
+				</div>
+			</div>
+
+			<div className='text-white justify-center mt-10'>
+				<div>
+					<div className='flex gap-x-4 justify-center pb-6'>
 						<InstagramButton />
 						<LinkedInButton />
 						<FacebookButton />
 						<TwitterButton />
 						<YoutubeButton />
 					</div>
-          </div>    
-      </div>
-  
-
-
-  <div className='text-white grid grid-cols-3 w-full'>
-				<div>
-					<p className='text-white font-normal pt-4'>
-						© Copyright 2023 SWOP All rights reserved.
+					<p className='font-semibold text-lg mt-5 pb-6 text-white text-center'>
+						SWOP
+					</p>
+					<p className='text-white font-normal pb-6 text-base text-center'>
+						{`© Copyright ${new Date().getFullYear()Swop Analysis All rights reserved.`}
 					</p>
 				</div>
-				<div></div>
-				<div></div>
 			</div>
 		</>
 	);
 }
 
 export default PageFooter;
-
-*/
